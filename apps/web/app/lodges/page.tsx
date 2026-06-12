77fbf696eb5bb34ac3db66bdcf6c6ca39a096b3b
Arjan Hoek <ahoekdev@gmail.com>
2026-06-12 07:58:15 +0200

    add lodges page

---
+++ apps/web/app/lodges/page.tsx
@@ -0,0 +1,7 @@
+export default function Lodges() {
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold">Lodges</h1>
+    </div>
+  );
+}
---
+++ apps/web/components/SiteHeader.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function SiteHeader() {
+  return (
+    <header className="container mx-auto px-4 h-16 flex items-center justify-between">
+      <nav>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/lodges">Lodges</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
